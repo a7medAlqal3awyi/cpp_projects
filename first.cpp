0ac1581961ea5ac1b7aaeb0d5ef33bf6782c40d8
Ahmed <a7medalqalawyi@bmail.com>
2023-07-20 19:11:02 +0300

    First program about calc memory

---
+++ first.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std ;
+int main(){ 
+    cout<<"Hello in my app\n";
+    cout<<"Enter the number in bit  \n";
+    int bit;
+    cin>>bit;
+    cout <<"the number in kilo byte="<<bit*8*1024<<"\n";
+    cout <<"the number in byte="<<bit*8<<"\n";
+
+
+    
+    
+    return 0 ; 
+
+}
