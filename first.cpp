c51e232d058fc7f7ab78b9d29fe6e6645eb077ba
Ahmed <a7medalqalawyi@bmail.com>
2023-07-20 19:31:00 +0300

    double and float  sizes

--- first.cpp
+++ first.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
-#include <iomanip>
-using namespace std ;
-int main(){ 
-    cout<<"Hello in my app\n";
-    cout<<"Enter the number in bit  \n";
-    int bit;
-    cin>>bit;
-    cout <<"the number in kilo byte="<<bit*8*1024<<"\n";
-    cout <<"the number in byte="<<bit*8<<"\n";
+using namespace std;
+int main()
+{
+    // data types ===> double
 
+    int num = 20.5;
+    cout << num << "\n";
+    cout << sizeof(num) << "\n";
+    cout << "*******************************************\n";
+    double num2 = 20.4;
+    cout << num2 << "\n";
+    cout << sizeof(num2) << "\n";
 
-    
-    
-    return 0 ; 
+    cout << "*******************************************\n";
 
+    float num3 = 20.5 + 5.9; // here compilier deal with nums as double and this will be slower than float to avoid this put 'f' affter each num like this
+
+    float f1 = 20.5f + 5.9f; // here deal as float
+    cout << num3 << "\n";
+    cout << sizeof(num3) << "\n";
+    cout << "*******************************************\n";
+
+    auto dol = 5.5; // deal as double
+    auto dd = 5.5f; // deal as float
+    cout << sizeof(dol) << "\n"; // 8
+    cout << sizeof(dd) << "\n";  // 4 
+    return 0;
 }
