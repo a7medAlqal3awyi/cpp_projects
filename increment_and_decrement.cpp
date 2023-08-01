80df1d57c2a2d43d65e319893d217c7d3bb26fca
Ahmed <a7medalqalawyi@bmail.com>
2023-08-01 23:54:46 +0300

    finish increment_and_decrement  v26

---
+++ increment_and_decrement.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int likes = 0;
+    cout << likes++ << "\n";
+    cout << likes << "\n";
+
+    cout<<"================"<<"\n";
+    cout<<"****************"<<"\n";
+
+    int views = 0;
+    cout << views-- << "\n";
+    cout << views << "\n";
+}
