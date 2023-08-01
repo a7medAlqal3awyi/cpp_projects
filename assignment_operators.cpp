e2263ccbf9ab3b02cf1438c71feb01c056feb80c
Ahmed <a7medalqalawyi@bmail.com>
2023-08-01 23:47:47 +0300

    finish v 25 assign operators

---
+++ assignment_operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    a += 5;
+    cout << a << "\n";
+    int b = 20;
+    b -= 10;
+    cout << b << "\n";
+    int c = 5;
+    c *= 5;
+
+    cout << c << "\n";
+}
