d91d9d4b51996057a22972d2f5196a87ecd31a40
Ahmed <a7medalqalawyi@bmail.com>
2023-08-05 13:14:06 +0300

    V34 Nested ternary operators

--- control_flow_if.cpp
+++ control_flow_if.cpp
@@ -5,25 +5,25 @@ int main()
     int age = 15;
     int points = 800;
     int grade = 8;
-    if (age >= 20)
-    {
-        cout << "your age is ok\n";
-    }
-    else if (points > 500)
-    {
-        cout << "your points is ok\n";
-    }
-    else if (grade > 5)
-    {
-        cout << "your grade is ok\n";
-    
-    }
-    else
-    {
-        cout << "Iam sorry \n";
-    }
+    // if (age >= 20)
+    // {
+    //     cout << "your age is ok\n";
+    // }
+    // else if (points > 500)
+    // {
+    //     cout << "your points is ok\n";
+    // }
+    // else if (grade > 5)
+    // {
+    //     cout << "your grade is ok\n";
 
-// ternary conditional
-cout<<(age>=18 ? "age is ok ":"age is not ok ");
+    // }
+    // else
+    // {
+    //     cout << "Iam sorry \n";
+    // }
+
+    //  Nested ternaryconditional
+    cout << (age >= 20 ? "age is ok " : (points > 1000 ? "yes ok points" : "No p "));
     return 0;
 }
