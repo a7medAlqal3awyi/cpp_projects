1adec00f1668086f113b731a09f903e90ae80093
Ahmed <a7medalqalawyi@bmail.com>
2023-08-08 07:26:10 +0300

    update element in array

--- control_flow_projs/switch/array/arrays.cpp
+++ control_flow_projs/switch/array/arrays.cpp
@@ -18,5 +18,26 @@ cout<<"location : "<<& arr[2]<<"\n";
 double dos[]{20 , 84.6 , 45.9 , 80 , 556.6};
 cout<<sizeof(dos); // 8 * 5 = 40
 
+
+cout<<"********************************"<<"\n";
+
+int num[4];
+num[0]=10;
+num[1]=20;
+num[2]=30;
+num[3]=40;
+
+cout <<"first element "<<num[0] <<"\n";
+cout <<"secind element "<<num[1] <<"\n";
+cout <<"third element "<<num[2] <<"\n";
+
+num[1]=2000;
+cout <<"secind element after update "<< num [1] <<"\n";
+cout<<"********************************"<<"\n";
+
+
+int ahmed[]={10,20,30,40,50,60};
+cout<<"the number of element "<<sizeof(ahmed)/4;
+
     return 0; 
 }
