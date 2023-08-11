5742b7f720f40e8ab806321bb4c001125c5df01c
Ahmed <a7medalqalawyi@bmail.com>
2023-08-11 10:27:08 +0300

    for projects

---
+++ loops/for/for_project.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int nums[] = {100, 200, 300, 400, 500, 600};
+
+    int numCounter = sizeof(nums) / sizeof(nums[0]);
+
+    // first project output is 100, 300, 500
+    // for (int i = 0; i < numCounter; i += 2)
+    // {
+    //     cout << nums[i] << "\n";
+    // }
+
+    // cout << "------- OR -------\n";
+    // for (int i = 0; i < numCounter; i++)
+    // {
+    //     if (i % 2 == 0)
+    //     {
+    //         cout << nums[i] << "\n";
+    //     }
+    // }
+
+    // cout << "------- OR -------\n";
+    // for (int i = 0; i < numCounter; i++)
+    // {
+
+    //     cout << nums[i] << "\n";
+    //     i++;
+    // }
+
+    // second project the output is 600,500,400,300
+    // cout << nums[5];
+
+
+    for (int i =numCounter-1; i >=2; i--)
+    {
+        cout << nums[i] << endl;
+    }
+    return 0;
+}
