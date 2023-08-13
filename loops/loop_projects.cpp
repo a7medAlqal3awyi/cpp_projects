980723d578244c04a232ddeff22fa931583d267f
Ahmed <a7medalqalawyi@bmail.com>
2023-08-13 09:56:26 +0300

    loops >>projects

---
+++ loops/loop_projects.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // first project to add  even and odd nums only
+    // int nums[] = {10, 20, 5, -6, 8, -7, 10, -11, 3};
+    // int length = sizeof(nums) / sizeof(nums[0]);
+    // int res, odd_nums, even_nums;
+    // for (int i = 0; i < length; i++)
+    // {
+    //     res += nums[i];
+    //     if (nums[i] % 2 == 0)
+    //     {
+    //         even_nums += nums[i];
+    //         // cout << res << '\n';
+    //     }
+    //     else
+    //     {
+    //         odd_nums += nums[i];
+    //     }
+    // }
+
+    // cout << " Total Even " << even_nums << '\n';
+    // cout << " Total Odd " << odd_nums << '\n';
+    // cout << " Total " << res << '\n';
+    //**************************************************************
+    // second project guesss number
+
+    //     int gussed_num = 8;
+    //     int numTries = 0, choose;
+
+    //     cout << "Enter yr number that u guess \n";
+
+    //     while (true)
+    //     {
+    //         cin >> choose;
+
+    //         if (choose == gussed_num)
+    //         {
+    //             cout << "Congratulation !!!!!!!1\n";
+    //             break;
+    //         }
+    //         else
+    //         {
+    //             cout << "u guess false \n";
+    //             numTries++;
+    //         }
+    //         if(numTries==3){
+    //             cout<<"Canot try more\n";
+    //             break;
+    //         }
+
+    //     }
+    //**************************************************************
+    // resversed numbers
+    int vals[5];
+    int n1, n2, n3, n4, n5, num;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Enter the nuber \n";
+        cin >> num;
+        vals[i] = num;
+    }
+    cout << "********\n";
+    for (int i = 4; i >= 0; i--)
+    {
+        cout << vals[i] << endl;
+    }
+}
