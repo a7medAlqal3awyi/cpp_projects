15ecd553ab89f1186f284e024e08103ae657a174
Ahmed <a7medalqalawyi@bmail.com>
2023-08-16 07:26:31 +0300

    built in function

---
+++ methods/built_in_meth.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    cout << pow(2, 4) << "\n";
+    cout << fmod(10.5, 2) << "\n";
+    cout << ceil(10.1) << "\n";
+    cout << floor(10.99) << "\n";
+    cout << round(10.499) << "\n";
+    cout << round(10.5) << "\n";
+    cout << trunc(50.5) << "\n";
+    cout << trunc(10.9) << "\n";
+    cout << trunc(70.1) << "\n";
+}
